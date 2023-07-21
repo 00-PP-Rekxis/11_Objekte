3d9b27943450ca4db37d3679a8bbac469c772fb5
Rekxis <Rekxis@gmx.de>
2023-07-21 11:51:10 +0200

    added object +  misc output

--- script_01.js
+++ script_01.js
@@ -20,6 +20,7 @@
 // output(arr[0]);
 // output(arr[0][0]+ " " + arr[1][2]);
 // output(arr[1][0]);
+// output("------------");
 
 // // nested loop
 // for (let i = 0; i < arr.length; i++) {
@@ -32,11 +33,31 @@
 
 /***** Objekte 1 Daten/Funktionen *******/
 
+let person = {
+    firstName: 'Bashkim',
+    familyName: 'Dauti',
+    salary: [120000, 160000],
+    permission: true,
+    sayHello: function(){
+        return 'Hallo, ' + this.firstName + "!";
+    }
+};
 
 
+output(person);
+output(person['firstName']);
+output(person.firstName);
+output(person.familyName);
+output(person.salary[1]);
+output(person.permission);
 
+const text =    "Ich bin " + person.firstName + 
+                " und verdiene " + person.salary[1] +
+                " p.a.";
 
+output(text);
 
+output(person.sayHello());
 
 /** Ausgabe */
 function output(outputData) {
