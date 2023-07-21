19a5159d36dccad63c97f1f4f19627dce5438771
Rekxis <Rekxis@gmx.de>
2023-07-21 12:17:08 +0200

    Objekte 2 - Hierarchie

--- script_01.js
+++ script_01.js
@@ -15,7 +15,7 @@
 //                 ["Ich","bin","Moritz"]
 //         ];
 
-        
+
 // output(arr);
 // output(arr[0]);
 // output(arr[0][0]+ " " + arr[1][2]);
@@ -33,31 +33,52 @@
 
 /***** Objekte 1 Daten/Funktionen *******/
 
-let person = {
-    firstName: 'Bashkim',
-    familyName: 'Dauti',
-    salary: [120000, 160000],
-    permission: true,
-    sayHello: function(){
-        return 'Hallo, ' + this.firstName + "!";
-    }
-};
+// let person = {
+//     firstName: 'Bashkim',
+//     familyName: 'Dauti',
+//     salary: [120000, 160000],
+//     permission: true,
+//     sayHello: function(){
+//         return 'Hallo, ' + this.firstName + "!";
+//     }
+// };
 
 
-output(person);
-output(person['firstName']);
-output(person.firstName);
-output(person.familyName);
-output(person.salary[1]);
-output(person.permission);
+// output(person);
+// output(person['firstName']);
+// output(person.firstName);
+// output(person.familyName);
+// output(person.salary[1]);
+// output(person.permission);
 
-const text =    "Ich bin " + person.firstName + 
-                " und verdiene " + person.salary[1] +
-                " p.a.";
+// const text =    "Ich bin " + person.firstName + 
+//                 " und verdiene " + person.salary[1] +
+//                 " p.a.";
 
-output(text);
+// output(text);
 
-output(person.sayHello());
+// output(person.sayHello());
+
+/***** Objekte 2 - Hierarchie *******/
+// Tiefer, tiefer - Irgendwo in der Tiefe gibt es ein Licht // K.Bush
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ...
+// deep - deeper - deepest
+// nested object {{{}}}
+
+let baikal = {
+    value: '10m',
+    deep: {
+        deeper: {
+            deepest: 'Das Licht - auf 1642m!',
+            temperatur: '4 Grad Celsius',
+        },
+    },
+}
+
+output(baikal);
+output(baikal.value);
+output(baikal.deep.deeper.deepest);
+output(baikal.deep.deeper.temperatur);
 
 /** Ausgabe */
 function output(outputData) {
